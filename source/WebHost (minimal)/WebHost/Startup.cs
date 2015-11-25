4ef8a2c2880c9a432bc95424875233f3e6e561b7
Tragedian <programming.hero@gmail.com>
2015-11-25 23:18:22 +0000

    Added OWIN response buffer

--- source/WebHost (minimal)/WebHost/Startup.cs
+++ source/WebHost (minimal)/WebHost/Startup.cs
@@ -5,6 +5,7 @@ using Owin;
 using Configuration;
 using IdentityServer3.Core.Configuration;
 using Serilog;
+using System.IO;
 
 [assembly: OwinStartup(typeof(WebHost.Startup))]
 
@@ -28,6 +29,21 @@ namespace WebHost
                 SigningCertificate = Certificate.Load(),
                 Factory = factory,
             };
+
+			appBuilder.Use(async (ctx, next) =>
+				{
+					// Buffer the output response.
+					var stream = ctx.Response.Body;
+					var buffer = new MemoryStream();
+					ctx.Response.Body = buffer;
+
+					await next();
+
+					ctx.Response.Body = stream;
+					ctx.Response.Headers["Content-Length"] = buffer.Length.ToString();
+					buffer.Seek(0, SeekOrigin.Begin);
+					await buffer.CopyToAsync(stream);
+				});
 
             appBuilder.UseIdentityServer(options);
         }
